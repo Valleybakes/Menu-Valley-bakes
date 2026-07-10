9e391e5e30699ae0a5f485fd8aa664c3db2b9da6
m8wgj5w4rc-commits <m8wgj5w4rc@privaterelay.appleid.com>
2026-07-10 01:49:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-// ===============================
-// Valley Bakes
-// ===============================
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
 
-// Mensaje de pedidos según el día
+let actual = 0;
+
+function cambiarSlide(){
+
+    slides[actual].classList.remove("active");
+    dots[actual].classList.remove("active-dot");
+
+    actual++;
+
+    if(actual >= slides.length){
+        actual = 0;
+    }
+
+    slides[actual].classList.add("active");
+    dots[actual].classList.add("active-dot");
+
+}
+
+setInterval(cambiarSlide,3500);
+
+// Mensaje de pedidos
 
 const estado = document.getElementById("estadoPedidos");
 
-if (estado) {
+if(estado){
 
     const hoy = new Date().getDay();
 
-    if (hoy === 3 || hoy === 6) {
+    if(hoy === 3 || hoy === 6){
 
         estado.innerHTML = "🟢 ¡Hoy estamos aceptando pedidos!";
 
-    } else {
+    }else{
 
-        estado.innerHTML = "📅 Los pedidos se toman los miércoles y sábados.";
+        estado.innerHTML = "📅 Recibimos pedidos los miércoles y sábados.";
 
     }
 
 }
-
-// Animación al cargar
-
-window.addEventListener("load", () => {
-
-    document.body.style.opacity = "1";
-
-});
